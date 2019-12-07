47c675bd19552dc7d397152c0e20f9b92cba9c94
topskilldev <me@topskill.dev>
2019-12-07 09:39:50 -0800

    API with React

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\TranGame;
 use App\Models\TranTransfer;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 
 class TransactionController extends Controller
 {
@@ -22,21 +23,30 @@ class TransactionController extends Controller
     }
 
     public function gamePlayed(Request $request) {
+        $page = $request->has('page') ? $request->page : 1;
+        $pageSize = $request->has('pageSize') ? $request->pageSize : 15;
+
+        Paginator::currentPageResolver(function () use ($page) {
+            return $page;
+        });
+
         if ($request->has('start') && $request->has('end')) {
             $start = strtotime($request->start . " 00:00:00") * 1000;
             $end = strtotime($request->end . "23:59:59") * 1000;
 
-            $count = TranGame::where('GAME_START', '>', $start)->where('GAME_START', '<', $end)->count();
+            return response()->json(TranGame::where('GAME_START', '>', $start)->where('GAME_START', '<', $end)->paginate($pageSize));
         } else {
             $start = 0; $end = 0;
 
-            $count = TranGame::all()->count();
+            return response()->json(TranGame::paginate($pageSize));
         }
 
+        /*
         return response()->json([
             'amountGamePlayed' => $count,
             'start' => $start,
             'end' => $end
         ]);
+        */
     }
 }
