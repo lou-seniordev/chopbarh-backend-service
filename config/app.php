0c2c2548fd55bfde81243036ad080b7a7ef2603b
topskilldev <me@topskill.dev>
2020-02-20 21:05:31 -0700

    cors issue

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Yajra\DataTables\DataTablesServiceProvider::class,
+	FruitCake\Cors\CorsServiceProvider::class,
     ],
 
     /*
